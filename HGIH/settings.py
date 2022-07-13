9b5763f0b5691070328806de146fc5df570a5fc4
Aashishkumar9 <92943237+Aashishkumar9@users.noreply.github.com>
2022-07-13 01:10:18 -1200

    Update settings.py

--- HGIH/settings.py
+++ HGIH/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ix2mv2vbsz*&!19_+g*8a_)x)yqjd36siufm!=1#nq3#tk!ja*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
